d36e554951ea7825017614fbe6baa0c68c3f9518
Kai Berggold <mail@kaiberggold.de>
2024-07-09 20:35:12 +0200

    first running version

---
+++ doc/state.md
@@ -0,0 +1,21 @@
+```Mermaid
+stateDiagram
+[*] --> Off
+  Off --> OnTransient: if (raw==on)
+  OnTransient --> OffTransientShort: if (raw==off)
+  OffTransientShort --> OnShort:if (raw==on)
+  state On {
+                [*] --> OnShort
+                OnLong
+            }
+
+  OnTransient --> On: after (DEBOUNCE_TIME_ON)<br>entry#colon; event_short()
+  OnShort -->OffTransientShort: if (raw==off)
+  OnShort--> OnLong: after(TIME_LONG)<br>entry#colon; event_long()
+  OffTransientShort -->Off: After (DEBOUNCE_TIME_OFF)
+  OnShort: "OnShort"
+  OnLong: "OnLong"
+  OnLong --> OffTransientLong: raw==off
+  OffTransientLong --> OnLong: raw==on
+  OffTransientLong --> Off: after (DEBOUNCE_TIME_OFF)
+```
--- include/input_events_impl.h
+++ include/input_events_impl.h
@@ -1,39 +1,31 @@
 #ifndef INPUT_EVENTS_IMPL_H
 #define INPUT_EVENTS_IMPL_H
 
+#include <iostream>
 #include "input_events_if.h"
 #include "input_actuators_events_if.h"
-#include <iostream>
+#include "timer_service_if.h"
+#include "button_state.h"
 
 class InputEventsImpl : public InputActuatorsEventsIf
 {
 private:
     InputEventsIf *_input_events;
+    TimerServiceIf *_timer_service;
     bool _state = false;
+    ButtonStateChart button;
 
 public:
-    InputEventsImpl(InputEventsIf *ie) : _input_events(ie)
+    InputEventsImpl(InputEventsIf *ie, TimerServiceIf *t)
+        : _input_events(ie), _timer_service(t), button(ButtonState::OFF, t)
+
     {
     }
 
     void setInputActuatorEvent(InputActuatorEventType inputActuatorEvent) override
     {
         std::cout << "reveived actuator Event " << static_cast<int>(inputActuatorEvent) << std::endl;
-        if (_state)
-        {
-            if (inputActuatorEvent == InputActuatorEventType::BUTTON_FALLING_EDGE)
-            {
-                _state = false;
-                _input_events->setInputEvent(InputEventType::ROTARY_ENCODER_1_LEFT_SLOW);
-            }
-        }
-        else
-        {
-            if (inputActuatorEvent == InputActuatorEventType::BUTTON_RISING_EDGE)
-            {
-                _state = true;
-            }
-        }
+        button.setNewActuatorEvent(inputActuatorEvent);
     }
 };
 
