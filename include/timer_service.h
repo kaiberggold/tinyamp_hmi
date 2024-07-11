449a8a4d93e67298ed8d90413b680c6e1fd3608d
Kai Berggold <mail@kaiberggold.de>
2024-07-11 09:37:30 +0200

    cleanup

--- include/timer_service.h
+++ include/timer_service.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "timer_service_if.h"
 #include <array>
+#include <algorithm>
 
 struct TimerEvent
 {
@@ -27,33 +28,47 @@ public:
     }
     void step() override
     {
-
-        std::for_each(arr.begin(), arr.end(), [&arr](int& element){
-        if (element.time > 0) {
-            element.time--;
-            const int index = std::distance(arr.begin(), &element);
-            if (element.time == 0) {
-                element.callback(index);
+        for (size_t i = 0; i < _events.size(); ++i)
+        {
+            auto &event = _events[i];
+            if (event.time > 0)
+            {
+                event.time--;
+                if (event.time == 0)
+                {
+                    std::cout << "Callback " << i << " triggered\n";
+                    event.callback(i);
+                }
             }
         }
     }
 
     int register_event(int time, CallbackFunc callback) override
     {
-        int id = 0;
-        while (_events[id].time > 0)
+        for (size_t id = 0; id < _events.size(); ++id)
         {
-            id++;
+            if (_events[id].time == 0)
+            {
+                _events[id].time = time;
+                _events[id].callback = callback;
+                return id;
+            }
         }
-        _events[id].time = time;
-        _events[id].callback = callback;
-        return id;
+        std::cerr << "Error: No available slots to register event." << std::endl;
+        return -1;
     }
 
     void delete_event(int id) override
     {
-        std::cout << "Deleted Event" << id << std::endl;
-        _events[id].time = 0;
+        if (id >= 0 && id < _events.size())
+        {
+            std::cout << "Deleted Event " << id << std::endl;
+            _events[id].time = 0;
+        }
+        else
+        {
+            std::cerr << "Error: Invalid event id " << id << std::endl;
+        }
     }
 };
 
